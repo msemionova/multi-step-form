e98e1192edc741431ee3f36ea1e3de9d1ba012ee
Maria Semionova <maria.semionova@spark.net>
2023-06-02 00:06:26 +0200

    Configure ESLint

--- src/components/organisms/RegistrationForm/RegistrationForm.tsx
+++ src/components/organisms/RegistrationForm/RegistrationForm.tsx
@@ -1,9 +1,6 @@
 import styles from './RegistrationForm.module.scss';
 
-export interface RegistrationFormProps {
-}
-
-export default function RegistrationForm(props: RegistrationFormProps) {
+export default function RegistrationForm() {
   return (
     <div className={styles['container']}>
       <div className={styles['sidebar']}></div>
