7ab5867000a35762a1f7f67c427d32864b3dda6c
Maria Semionova <maria.semionova@spark.net>
2023-06-01 23:08:28 +0200

    Initial setup

---
+++ src/components/organisms/RegistrationForm/RegistrationForm.tsx
@@ -0,0 +1,13 @@
+import styles from './RegistrationForm.module.scss';
+
+export interface RegistrationFormProps {
+}
+
+export default function RegistrationForm(props: RegistrationFormProps) {
+  return (
+    <div className={styles['container']}>
+      <div className={styles['sidebar']}></div>
+
+    </div>
+  );
+}
